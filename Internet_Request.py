6f301f53d0efc498b44e521eaae7b19dd4b73cdd
Littlemimss <111756931+Littlemimss@users.noreply.github.com>
2022-09-14 13:20:21 +0300

    Add files via upload

---
+++ Internet_Request.py
@@ -0,0 +1,28 @@
+import requests
+from winotify import Notification
+
+
+def get_price():
+    site_html = requests.get("https://www.emag.bg/smartfon-apple-iphone-13-256gb-5g-red-mlq93rm-a/pd/DSFCMXMBM/?").text
+
+    split_html = site_html.split(';')
+
+    #print(split_html)
+
+    for part in split_html:
+        if 'EM.productFullPrice' in part:
+            price = part.split()[2]
+            return float(price)
+
+
+price = get_price()
+print(price)
+
+if price < 1750:
+    notifier = Notification(
+        app_id="Price checker",
+        title='iPhone price checker',
+        msg='Buy the iPhone!',
+        duration='short'
+    )
+    notifier.show()
